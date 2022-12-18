007e43022c07b0858e4f7bdcfe366770a3098017
loket <loket@cruftlab.com>
2022-12-18 10:13:37 +0100

    solve day 4 part 1

---
+++ src/aoc/day04.py
@@ -0,0 +1,36 @@
+import typing
+from typing import Any, Generator, List
+
+from src.aoc import solve
+
+
+def assignment_pair(text: str) -> list[list[int]]:
+    """
+    >>> assignment_pair("2-4,6-8")
+    [[2, 3, 4], [6, 7, 8]]
+    """
+    pair_str = text.split(",")
+    assignment_str = [p.split("-") for p in pair_str]
+    return [list(range(int(a), int(b) + 1)) for a, b in assignment_str]
+
+
+def is_fully_contained(a: list[int], b: list[int]) -> bool:
+    a_in_b = all(n in b for n in a)
+    b_in_a = all(n in a for n in b)
+    return a_in_b or b_in_a
+
+
+class Day04:
+    @staticmethod
+    def part_one(text: str) -> typing.Optional[int]:
+        pairs = [assignment_pair(line) for line in text.splitlines()]
+        return sum(is_fully_contained(a, b) for a, b in pairs)
+
+    @staticmethod
+    def part_two(text: str) -> typing.Optional[int]:
+        return None
+
+
+if __name__ == '__main__':
+    solve(4, 1, Day04.part_one)
+    solve(4, 2, Day04.part_two)
---
+++ src/aoc/test_day04.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from src.aoc.day04 import Day04
+
+
+class TestDay04(TestCase):
+    input = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+    def test_part_one(self):
+        result = Day04.part_one(self.input)
+        self.assertEqual(result, 2)
+
+    def test_part_two(self):
+        result = Day04.part_two(self.input)
+        self.assertEqual(result, None)
